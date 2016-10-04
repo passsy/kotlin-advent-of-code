df8f09b0748305f41caf16ffec8ea003ec39540d
Pascal Welsch <pascal.welsch@gmail.com>
2016-10-04 15:27:29 +0200

    Solved day2-2

--- src/test/kotlin/day2/ThereIsMathTest.kt
+++ src/test/kotlin/day2/ThereIsMathTest.kt
@@ -24,9 +24,20 @@ class ThereIsMathTest : Spek({
         assertThat(result).isEqualTo(1598415)
     }
 
-    test("real example") {
-        TODO()
-        val file = File("in/day2-2")
-        val input = file.readLines()[0]
+    describe("calculate ribbon") {
+
+        test("A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.") {
+            assertThat(requiredRibbon("2x3x4")).isEqualTo(34)
+        }
+
+        test("A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.") {
+            assertThat(requiredRibbon("1x1x10")).isEqualTo(14)
+        }
+    }
+
+    test("combined ribbon required") {
+        val input = File("in/day2-2").readLines()
+        val result = input.map { requiredRibbon(it) }.sum()
+        assertThat(result).isEqualTo(3812909)
     }
 })

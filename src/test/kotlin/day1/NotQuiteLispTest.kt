c6cb5adad8508a37bbf3d6f3ef08b560a4bf31bd
Pascal Welsch <pascal.welsch@gmail.com>
2016-10-04 11:55:32 +0200

    Use correct syntax to define spek tests

--- src/test/kotlin/day1/NotQuiteLispTest.kt
+++ src/test/kotlin/day1/NotQuiteLispTest.kt
@@ -3,36 +3,41 @@ package day1
 import org.assertj.core.api.KotlinAssertions.assertThat
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
+import java.io.File
+
 
 class NotQuiteLispTest : Spek({
 
-    describe("(()) and ()() both result in floor 0.") {
-        assertThat(whatFloor("(())")).isEqualTo(0)
-        assertThat(whatFloor("()()")).isEqualTo(0)
+    describe("examples") {
+
+        test("(()) and ()() both result in floor 0.") {
+            assertThat(whatFloor("(())")).isEqualTo(0)
+            assertThat(whatFloor("()()")).isEqualTo(0)
+        }
+
+        test("((( and (()(()( both result in floor 3.") {
+            assertThat(whatFloor("(((")).isEqualTo(3)
+            assertThat(whatFloor("(()(()(")).isEqualTo(3)
+        }
+
+        test("))((((( also results in floor 3.") {
+            assertThat(whatFloor("))(((((")).isEqualTo(3)
+        }
+
+        test("()) and ))( both result in floor -1 (the first basement level).") {
+            assertThat(whatFloor("())")).isEqualTo(-1)
+            assertThat(whatFloor("))(")).isEqualTo(-1)
+        }
+
+        test("))) and )())()) both result in floor -3.") {
+            assertThat(whatFloor(")))")).isEqualTo(-3)
+            assertThat(whatFloor(")())())")).isEqualTo(-3)
+        }
     }
 
-    describe("((( and (()(()( both result in floor 3.") {
-        assertThat(whatFloor("(((")).isEqualTo(3)
-        assertThat(whatFloor("(()(()(")).isEqualTo(3)
-    }
-
-    describe("))((((( also results in floor 3.") {
-        assertThat(whatFloor("))(((((")).isEqualTo(3)
-    }
-
-    describe("()) and ))( both result in floor -1 (the first basement level).") {
-        assertThat(whatFloor("())")).isEqualTo(-1)
-        assertThat(whatFloor("))(")).isEqualTo(-1)
-    }
-
-    describe("))) and )())()) both result in floor -3.") {
-        assertThat(whatFloor(")))")).isEqualTo(-3)
-        assertThat(whatFloor(")())())")).isEqualTo(-3)
-    }
-
-    /*describe("real example input 1") {
+    test("real example input 1") {
         val file = File("in/day1-1")
-        val input file.readLines()[0]
+        val input = file.readLines()[0]
         assertThat(whatFloor(input)).isEqualTo(138)
-    }*/
+    }
 })

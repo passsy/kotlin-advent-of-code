38af2a4614c9ceb3c77f9dc6932d33d9df0e3fe4
Pascal Welsch <pascal.welsch@gmail.com>
2017-12-05 11:30:34 +0100

    Solve day 5

---
+++ src/test/kotlin/y2017/5_TwistyTrampolinesMaze.kt
@@ -0,0 +1,28 @@
+package y2017
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+
+class TwistyTrampolinesMaze {
+
+    @Test
+    fun `small jump sample, exit at end`() {
+        assertThat(stepCount(listOf(0, 3, 0, 1, -3))).isEqualTo(5)
+    }
+
+    @Test
+    fun `exit at beginning`() {
+        assertThat(stepCount(listOf(1, 2, 0, -6))).isEqualTo(3)
+    }
+
+    @Test
+    fun `immediate exit`() {
+        assertThat(stepCount(listOf(-1))).isEqualTo(1)
+    }
+
+    @Test
+    fun `small stranger jump sample, exit at end`() {
+        assertThat(stepCount2(listOf(0, 3, 0, 1, -3))).isEqualTo(10)
+    }
+}

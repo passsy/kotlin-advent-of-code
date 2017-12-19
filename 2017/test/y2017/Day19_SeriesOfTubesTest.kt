4395667634d31e12133e78d117936d071eb1e2c9
Pascal Welsch <pascal.welsch@gmail.com>
2017-12-20 00:28:10 +0100

    Add missing tests

--- 2017/test/y2017/Day19_SeriesOfTubesTest.kt
+++ 2017/test/y2017/Day19_SeriesOfTubesTest.kt
@@ -7,7 +7,6 @@ import y2017.Day19_SeriesOfTubes.followRoutingDiagram
 
 class Day19_SeriesOfTubesTest {
 
-
     @Test
     fun `sample 1`() {
         val graph = """
@@ -19,7 +18,32 @@ class Day19_SeriesOfTubesTest {
             |     +B-+  +--+
             """.replaceIndentByMargin().drop(0).lines().map { it.toList() }
 
-        assertThat(followRoutingDiagram(graph)).isEqualTo("ABCDEF".toList())
-
+        assertThat(followRoutingDiagram(graph).letters).isEqualTo("ABCDEF")
     }
+
+    @Test
+    fun `solve part 1`() {
+        assertThat(Day19_SeriesOfTubes.part1.test()).isEqualTo("MKXOIHZNBL")
+    }
+
+    @Test
+    fun `sample 2`() {
+        val graph = """
+            |     |
+            |     |  +--+
+            |     A  |  C
+            | F---|----E|--+
+            |     |  |  |  D
+            |     +B-+  +--+
+            """.replaceIndentByMargin().drop(0).lines().map { it.toList() }
+
+        assertThat(followRoutingDiagram(graph).steps).isEqualTo(38)
+    }
+
+
+    @Test
+    fun `solve part 2`() {
+        assertThat(Day19_SeriesOfTubes.part2.test()).isEqualTo("17872")
+    }
+
 }
